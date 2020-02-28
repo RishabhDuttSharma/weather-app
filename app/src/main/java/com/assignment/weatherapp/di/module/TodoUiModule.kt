4db607fd34e4dc43ab0e335d512ff0446863ab85
Rishabh Dutt Sharma <rishabh.sharma03@nagarro.com>
2020-02-28 11:55:20 +0530

    added ui components in ui module

--- app/src/main/java/com/assignment/weatherapp/di/module/TodoUiModule.kt
+++ app/src/main/java/com/assignment/weatherapp/di/module/TodoUiModule.kt
@@ -1,6 +1,16 @@
 package com.assignment.weatherapp.di.module
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.assignment.weatherapp.ui.weatherinfo.WeatherInfoActivity
+import com.assignment.weatherapp.ui.weatherinfo.WeatherInfoFragment
+import com.assignment.weatherapp.ui.weatherinfo.WeatherInfoViewModel
+import com.assignment.weatherapp.util.viewmodel.DaggerViewModelFactory
+import com.nagarro.assignment.util.viewmodel.ViewModelKey
+import dagger.Binds
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
 
 /**
  *
@@ -10,17 +20,17 @@ import dagger.Module
 @Module
 abstract class TodoUiModule {
 
-//    @ContributesAndroidInjector
-//    abstract fun contributesTodoListActivity(): TodoListActivity
-//
-//    @ContributesAndroidInjector
-//    abstract fun contributesTodoListFragment(): TodoListFragment
-//
-//    @Binds
-//    abstract fun bindsDaggerViewModelFactory(viewModelFactory: DaggerViewModelFactory): ViewModelProvider.Factory
-//
-//    @Binds
-//    @IntoMap
-//    @ViewModelKey(TodoListViewModel::class)
-//    abstract fun bindsTodoListViewModel(viewModel: TodoListViewModel): ViewModel
+    @ContributesAndroidInjector
+    abstract fun contributesWeatherInfoActivity(): WeatherInfoActivity
+
+    @ContributesAndroidInjector
+    abstract fun contributesWeatherInfoFragment(): WeatherInfoFragment
+
+    @Binds
+    abstract fun bindsDaggerViewModelFactory(viewModelFactory: DaggerViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(WeatherInfoViewModel::class)
+    abstract fun bindsTodoListViewModel(viewModel: WeatherInfoViewModel): ViewModel
 }
